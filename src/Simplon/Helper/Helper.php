9d54c07f8f6a51dad9ee48dc20dee35f29d4f0ea
Tino Ehrich <ehrich@efides.com>
2013-05-02 18:01:41 +0200

    lower cased method

--- src/Simplon/Helper/Helper.php
+++ src/Simplon/Helper/Helper.php
@@ -193,7 +193,7 @@
          */
         public static function stringUrlable($string = FALSE, $slug = '-', $extra = NULL)
         {
-            return strtolower(trim(preg_replace('~[^0-9a-z' . preg_quote($extra, '~') . ']+~i', $slug, self::Unaccent($string)), $slug));
+            return strtolower(trim(preg_replace('~[^0-9a-z' . preg_quote($extra, '~') . ']+~i', $slug, self::unaccent($string)), $slug));
         }
 
         // ##########################################
@@ -205,7 +205,7 @@
          *
          * @return string
          */
-        public static function Unaccent($string)
+        public static function unaccent($string)
         {
             if (strpos($string = htmlentities($string, ENT_QUOTES, 'UTF-8'), '&') !== FALSE)
             {
